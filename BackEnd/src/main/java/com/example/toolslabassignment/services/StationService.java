2d8ec236abfdf08a1d78ed84bb642596ec7eeac5
MahmouDMustafa60331 <94802915+MahmouDMustafa60331@users.noreply.github.com>
2022-11-11 18:59:23 +0200

    adding deleteStation and UpdateStation

--- BackEnd/src/main/java/com/example/toolslabassignment/services/StationService.java
+++ BackEnd/src/main/java/com/example/toolslabassignment/services/StationService.java
@@ -1,6 +1,8 @@
 package com.example.toolslabassignment.services;
 
-import com.example.toolslabassignment.entities.Trip;
+
+import com.example.toolslabassignment.entities.Station;
+
 import com.example.toolslabassignment.repositories.stationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,27 @@ public class StationService {
 
     public List<Station> getAllStations(){
         return SRepo.findAll();
+    }
+
+    public void deleteStation(Long id){
+        SRepo.deleteById(id);
+    }
+
+    public String updateStation(Station s , Long id)
+    {
+
+            for(Station x : SRepo.findAll())
+            {
+                if(x.getId()==id)
+                {
+                    x.setName(s.getName());
+                    SRepo.save(x);
+                    return "Station is updated successfully";
+                }
+            }
+
+
+        return "no such Station";
     }
 
 
