8731f3fd270a80a08f9f560e7b424850d2572757
Abdallahhhassan <88851843+Abdallahhhassan@users.noreply.github.com>
2022-11-11 20:10:17 +0200

    Update app-routing.module.ts

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { StationsListComponent } from './stations-list/stations-list.component';
 import { UpdateTripComponent } from './update-trip/update-trip.component';
 import { UpdateAdminComponent } from './update-admin/update-admin.component';
 import { UpdateStationComponent } from './update-station/update-station.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 const routes: Routes = [
   {path : 'Trip',component: TripsListComponent},
   {path : 'Admin',component: AdminsComponent},
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path : 'Update-Trip',component: UpdateTripComponent},
   {path : 'Update-Station',component: UpdateStationComponent},
   {path : 'Update-Admin',component: UpdateAdminComponent},
+  {path : 'SignUp',component: SignUpComponent},
   {path : 'home',component: HomeComponent},
   {path: '',redirectTo:'home',pathMatch:'full'}
 ];
