8bac90ae7abf808f7534443487bc17e5985ee10f
Abdallahhhassan <88851843+Abdallahhhassan@users.noreply.github.com>
2022-11-11 19:54:50 +0200

    Update app-routing.module.ts

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TripsListComponent } from './trips-list/trips-list.component';
 import { AdminsComponent } from './admins-list/admins.component';
-
+import { StationsListComponent } from './stations-list/stations-list.component';
 
 const routes: Routes = [
   {path : 'Trip',component: TripsListComponent},
   {path : 'Admin',component: AdminsComponent},
+  {path : 'station',component: StationsListComponent},
 ];
 
 @NgModule({
