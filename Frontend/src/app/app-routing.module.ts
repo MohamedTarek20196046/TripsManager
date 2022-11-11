4a090b75e1fea10d2483d4380abb497704da9ff6
MohamedTarek20196046 <94565639+MohamedTarek20196046@users.noreply.github.com>
2022-11-11 20:00:46 +0200

    Update app-routing.module.ts

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { TripsListComponent } from './trips-list/trips-list.component';
 import { AdminsComponent } from './admins-list/admins.component';
 import { StationsListComponent } from './stations-list/stations-list.component';
-
+import { UpdateTripComponent } from './update-trip/update-trip.component';
 const routes: Routes = [
   {path : 'Trip',component: TripsListComponent},
   {path : 'Admin',component: AdminsComponent},
   {path : 'station',component: StationsListComponent},
+  {path : 'Update-Trip',component: UpdateTripComponent},
 ];
 
 @NgModule({
