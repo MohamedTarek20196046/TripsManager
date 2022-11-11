7cdd81c1e80ddc01862617017ab5f50132d5709c
Abdallahhhassan <88851843+Abdallahhhassan@users.noreply.github.com>
2022-11-11 19:03:42 +0200

    Adding TripController

---
+++ BackEnd/src/main/java/com/example/toolslabassignment/Controllers/TripController.java
@@ -0,0 +1,37 @@
+package com.example.toolslabassignment.Controllers;
+
+import com.example.toolslabassignment.entities.Trip;
+import com.example.toolslabassignment.services.TripService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Trip")
+@CrossOrigin(origins = "http://localhost:4200/")
+public class TripController {
+    @Autowired
+    TripService tripservice;
+
+    @PostMapping("/add")
+    public String createTrip(@RequestBody Trip t){
+       return tripservice.addtrip(t);
+    }
+
+    @GetMapping("/get/{id}")
+    public Trip getTripById(@PathVariable Long id){
+        return tripservice.getTrip(id);
+    }
+
+    @GetMapping("/getall")
+    public List<Trip> getAllTrips(){
+        return tripservice.getAlltrips();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteTrip(@PathVariable Long id){
+        tripservice.deleteTrip(id);
+    }
+
+}
