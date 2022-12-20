f654f3d6ff5a7f6f94e6b0cb080e008686984324
MahmouDMustafa60331 <94802915+MahmouDMustafa60331@users.noreply.github.com>
2022-12-20 18:28:51 +0200

    Update TripController.java

--- BackEnd/src/main/java/com/example/toolslabassignment/Controllers/TripController.java
+++ BackEnd/src/main/java/com/example/toolslabassignment/Controllers/TripController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/Trip")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class TripController {
     @Autowired
     TripService tripservice;
