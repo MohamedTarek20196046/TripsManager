cdf59422d6386eaf1585d95153ba55cea7072090
MahmouDMustafa60331 <94802915+MahmouDMustafa60331@users.noreply.github.com>
2022-12-20 18:29:01 +0200

    Update stationController.java

--- BackEnd/src/main/java/com/example/toolslabassignment/Controllers/stationController.java
+++ BackEnd/src/main/java/com/example/toolslabassignment/Controllers/stationController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/station")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class stationController {
     @Autowired
     StationService stationservice;
